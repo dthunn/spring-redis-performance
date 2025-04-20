0462be27f6f6ec5ca363598d0c4f5b626eabbcc9
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-04-20 15:02:31 -0600

    init

---
+++ src/main/java/com/dthunn/redisperformance/RedisperformanceApplication.java
@@ -0,0 +1,13 @@
+package com.dthunn.redisperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisperformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisperformanceApplication.class, args);
+	}
+
+}
